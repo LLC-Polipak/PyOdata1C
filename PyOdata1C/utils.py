dc788512e958597a52698aa6676095c722454aad
Dmitrii Glushenko <warstream55@gmail.com>
2024-09-05 17:44:58 +0300

    Добавлена логика для повышение приоритета операций

--- PyOdata1C/utils.py
+++ PyOdata1C/utils.py
@@ -1,8 +1,12 @@
-from .fields import FilterResultField
+from .fields import Field, FilterResultField
 
 
 def _not(s: FilterResultField):
     return FilterResultField(f"not({s})")
+
+
+def q(s: FilterResultField | Field):
+    return s.__class__(f'({s})')
 
 
 def isof():
